dac3b1b0777f45692fa31d389493346b1e4978f1
Francisco Giordano <frangio.1@gmail.com>
2022-02-01 20:40:50 -0300

    update tests

--- src/plugin.test.ts
+++ src/plugin.test.ts
@@ -3,7 +3,6 @@
 
 import test from 'ava';
 import { BigNumber } from 'ethers';
-// import { BN } from 'bn.js';
 import hre from 'hardhat';
 
 test('ethers', async t => {
@@ -14,22 +13,23 @@ test('ethers', async t => {
   const XWithVars = await hre.ethers.getContractFactory('XWithVars');
   const withVars = await XWithVars.deploy();
   t.is((await withVars.xvar1()).toNumber(), 55);
-  t.deepEqual((await withVars.xvar2()), [BigNumber.from(1), BigNumber.from(2), BigNumber.from(3)]);
+  t.deepEqual(await withVars.xvar2(), [BigNumber.from(1), BigNumber.from(2), BigNumber.from(3)]);
   t.is((await withVars.xvar3(1)), 2);
-  t.deepEqual((await withVars.xvar4()).a, BigNumber.from(1));
-  // t.deepEqual((await withVars.xvar5()), TODO);
-  t.deepEqual((await withVars.xvar6(1)).a, BigNumber.from(2));
-  t.deepEqual((await withVars.xvar7(1, true)).a, BigNumber.from(10));
-  // t.deepEqual((await withVars.xvar8(1, true)), TODO);
-
+  t.like(await withVars.xvar4(), { a: BigNumber.from(1) });
+  t.like(await withVars.xvar5(), {
+    0: { a: BigNumber.from(1) },
+    1: { a: BigNumber.from(2) },
+    2: { a: BigNumber.from(3) },
+  });
+  t.like(await withVars.xvar6(1), { a: BigNumber.from(2) });
+  t.like(await withVars.xvar7(1, true), { a: BigNumber.from(10) });
+  t.like(await withVars.xvar8(1, true), {
+    0: { a: BigNumber.from(11) },
+  });
 });
 
 test('truffle', async t => {
   const XFoo = hre.artifacts.require('XFoo');
   const foo = await XFoo.new();
   t.is((await foo.x_testFoo()).toString('hex'), 'f00');
-
-  const XWithVars = hre.artifacts.require('XWithVars');
-  const withVars = await XWithVars.new();
-  // TODO
 });
