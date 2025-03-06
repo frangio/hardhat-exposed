e246669e7c0cdb97ed42c80ffbf73d5ad33c1db7
Nick Barry <itsnickbarry@protonmail.ch>
2025-03-06 17:11:47 -0600

    add prefix type to ExposedConfig, move default prefix to extendConfig

--- src/plugin.ts
+++ src/plugin.ts
@@ -14,6 +14,7 @@ import type {} from './type-extensions';
 extendConfig((config, { exposed: userConfig }) => {
   config.exposed = {
     ...userConfig,
+    prefix: userConfig?.prefix ?? '$',
     exclude: userConfig?.exclude ?? [],
     include: userConfig?.include ?? ['**/*'],
     outDir: userConfig?.outDir ?? "contracts-exposed",
